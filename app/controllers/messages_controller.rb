d370625edd6b636324a458fb9372841241f41e54
orangepuer <rkhpuer@gmail.com>
2020-06-22 23:39:47 +0300

    Update MessagesController, implement #create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def create
+    current_user.messages.create(message_params)
+
+    redirect_to root_path
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
