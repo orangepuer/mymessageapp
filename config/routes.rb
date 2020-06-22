96aa7c99e44e5d0173dea33c62b38904d357694d
orangepuer <rkhpuer@gmail.com>
2020-06-22 20:02:47 +0300

    Add MessagesController and #create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :messages, only: [:create]
 end
