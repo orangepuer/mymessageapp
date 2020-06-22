c250bab098c7892e94e151bcd31e0e2c2b9416d2
orangepuer <rkhpuer@gmail.com>
2020-06-22 22:10:10 +0300

    Add migration AddUserToMessages and add association between User and Message models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_163224) do
+ActiveRecord::Schema.define(version: 2020_06_22_183812) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2020_06_22_163224) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
