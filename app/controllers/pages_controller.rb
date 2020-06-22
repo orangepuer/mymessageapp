c43c9b0eb11d004a7071896438122d2bd58d9c6a
orangepuer <rkhpuer@gmail.com>
2020-06-22 20:08:40 +0300

    Add PagesController and #home action

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+    @messages = Message.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  
+
+  root to: 'pages#home'
   resources :messages, only: [:create]
 end
