8ed280909a863b23ff4a4809eb3b45006edcf78f
orangepuer <rkhpuer@gmail.com>
2020-06-23 17:46:27 +0300

    Implement flash_error_messages helper method

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,9 @@
 module MessagesHelper
+  def flash_error_messages(resource)
+    flash['error_messages'] = []
+
+    resource.errors.full_messages.each do |message|
+      flash['error_messages'] << message
+    end
+  end
 end
