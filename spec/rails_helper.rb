8df897e5acbc426b23b9514fd16050cc368f0be3
orangepuer <rkhpuer@gmail.com>
2020-06-26 19:32:43 +0300

    Add AcceptanceHelper module for acceptance testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,6 +25,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
+  config.include AcceptanceHelper, type: :feature
 end
 
 Shoulda::Matchers.configure do |config|
